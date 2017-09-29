4a8737fb2e8b551384f679abd8101b3f25b9150f
samuraikun <tazitaziawawa@gmail.com>
2017-09-29 20:36:01 +0900

    API KEY をenvファイルに分ける

--- youtube_client.py
+++ youtube_client.py
@@ -2,12 +2,13 @@
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
+import settings
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyA6VwgvtIhzEJSb8BKWcSb0CXcEWndRZHU"
+DEVELOPER_KEY = settings.API_KEY
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
