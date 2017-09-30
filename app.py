66091c270ccbe50d3537caf68e27455bac9383f6
samuraikun <tazitaziawawa@gmail.com>
2017-10-01 02:58:29 +0900

    add vue.js

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import typing, json
-from apistar import Include, Route
+from apistar import Include, Route, annotate
+from apistar.interfaces import Templates
+from apistar.renderers import HTMLRenderer
 from apistar.frameworks.wsgi import WSGIApp as App
 from apistar.handlers import docs_urls, static_urls
 from apistar import typesystem
@@ -16,10 +18,10 @@ class Query(typesystem.String):
 class Order(typesystem.Enum):
     enum = ['date', 'rating', 'relevance', 'title', 'videoCount', 'viewCount']
 
-def welcome(name=None):
-    if name is None:
-        return {'message': 'Welcome to API Star!'}
-    return {'message': 'Welcome to API Star, %s!' % name}
+@annotate(renderers=[HTMLRenderer()])
+def welcome(username: str, templates: Templates):
+    index = templates.get_template('index.html')
+    return index.render(username=username)
 
 def search_video(query: Query, order_by: Order):
     if query is None:
@@ -37,7 +39,18 @@ routes = [
     Include('/static', static_urls)
 ]
 
-app = App(routes=routes)
+settings = {
+    'TEMPLATES': {
+        'ROOT_DIR': 'templates',
+        'PACKAGE_DIRS': ['apistar']
+    },
+    'STATICS': {
+        'ROOT_DIR': 'static',       # Include the 'static/' directory.
+        'PACKAGE_DIRS': ['apistar']  # Include the built-in apistar static files.
+    }
+}
+
+app = App(routes=routes, settings=settings)
 
 
 if __name__ == '__main__':
